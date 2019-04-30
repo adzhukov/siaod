ea57611f1678cfbb64e2adab3ec97224de7272c0
Alexander Zhukov <36170455+adzhukov@users.noreply.github.com>
2019-04-30 23:44:04 +0300

    Add t2_1_1.c

---
+++ t2_1_1.c
@@ -0,0 +1,116 @@
+/*
+                 = 287
+                  +
+               /     \
+              *        /
+            /  \     /   \
+           11   -   13     7
+               /  \
+             19    -
+                  /  \
+                 7    NULL
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+    struct node *left;
+    struct node *right;
+    int value;
+};
+
+typedef struct ExprTree {
+    struct node *root;
+} ExprTree;
+
+ExprTree *makeTestTree();
+void freeExprTree(ExprTree*);
+void freeExprTreeNode(struct node*);
+double evalNode(struct node*);
+double evalTree(ExprTree*);
+
+struct node *newValueNode(int value) {
+    struct node *ret = malloc(sizeof(struct node));
+    ret->left = ret->right = NULL;
+    ret->value = value;
+    return ret;
+}
+
+struct node *newOpNode(int op, struct node *left, struct node *right) {
+    struct node *ret = malloc(sizeof(struct node));
+    ret->left = left;
+    ret->right = right;
+    ret->value = op;
+    return ret;
+}
+
+ExprTree *makeTestTree() {
+    ExprTree *ret = malloc(sizeof(ExprTree));
+    ret->root = newOpNode('+',
+                          newOpNode('*',
+                                    newValueNode(11),
+                                    newOpNode('-',
+                                              newValueNode(19),
+                                              newOpNode('-',
+                                                        newValueNode(7),
+                                                        NULL))),
+                          newOpNode('/',
+                                    newValueNode(13),
+                                    newValueNode(7)));
+    return ret;
+}
+
+void freeExprTreeNode(struct node *node) {
+    if (node->left)
+        freeExprTreeNode(node->left);
+    if (node->right)
+        freeExprTreeNode(node->right);
+    free(node);
+}
+
+void freeExprTree(ExprTree *tree) {
+    if (tree->root) {
+        freeExprTreeNode(tree->root);
+    }
+    free(tree);
+}
+
+double evalNode(struct node *node) {
+    if (!node->left && !node->right)
+        return node->value;
+    switch (node->value) {
+        case '+':
+            return evalNode(node->left) + evalNode(node->right);
+            break;
+        case '*':
+            return evalNode(node->left) * evalNode(node->right);
+            break;
+        case '/':
+            return evalNode(node->left) / evalNode(node->right);
+            break;
+        case '-': {
+            if (!node->left)
+                return -evalNode(node->right);
+            if (!node->right)
+                return -evalNode(node->left);
+            return evalNode(node->left) - evalNode(node->right);
+            break;
+        }
+        default:
+            break;
+    }
+    return 0.0;
+}
+
+double evalTree(ExprTree *tree) {
+    return evalNode(tree->root);
+}
+
+int main(int argc, char **argv) {
+    ExprTree *tree = makeTestTree();
+    double result = evalTree(tree);
+    printf("result: %lf\n", result);
+    freeExprTree(tree);
+    return 0;
+}
